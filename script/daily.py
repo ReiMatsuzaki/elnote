f5efb3aa4541b41190ebf60ae30de2c8b1000787
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2017-12-18 09:10:49 +0900

    modify output location

--- script/daily.py
+++ script/daily.py
@@ -1,15 +1,13 @@
 import os
 exists = os.path.exists
 from subprocess import check_output
-from notelib import get_now
+from notelib import get_dirs
 
 def run():
-    interval = 100
-    d = get_now()
-    print d
+    interval = 1
+    d = get_dirs([0])[0]
     while True:
         check_output(["sleep", str(interval)])
-        print 1
         if(not exists(d)):            
             print "make directory for today:", d
             os.makedirs(d)
--- script/notemk.py
+++ script/notemk.py
@@ -15,25 +15,28 @@ def run():
     parser.add_argument("-n", "--name", default="note")
     args = parser.parse_args()
     notename = args.name
-    build_dir = join(get_root(), "build", notename)
+    # build_dir = join(get_root(), "build", notename)
+    build_dir = join(get_root(), "build")
     
     interval = 1
     notefiles = get_notefiles(args.tags)            
     last = [0 for nf in notefiles]
     os.chdir(NoteRoot)
 
+    """
     if(not exists(build_dir)):
         os.makedirs(build_dir)
     copyfile(join(NoteRoot, "config/note.tex"),
              join(build_dir, "note.tex"))
+    """
     
     while True:
-        print 1
         check_output(["sleep", str(interval)])
         current = get_timestamp(notefiles)
         if(last!=current):
-            cmd = ["pandoc"] + notefiles + ["-o", join(NoteRoot, notename+".html")]
-            print cmd
+            cmd = ["pandoc"] + notefiles + ["-o", join(build_dir, notename+".html")]
+            print "update notebook."
+            print "tags=", args.tags
             try:
                 check_output(cmd)
             except(Exception) as e:
